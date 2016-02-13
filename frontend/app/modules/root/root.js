8b70faf93179e6c2ac239229c4c3e986c67377fe
Luke Shellhorn <luke@lowgravityday.com>
2016-02-12 23:54:49 -0800

    updated fonts, font awesome

--- frontend/app/modules/root/root.js
+++ frontend/app/modules/root/root.js
@@ -9,6 +9,9 @@ angular.module('myApp.root', [])
 	// });
 }])
 
-.controller('RootCtrl', [function() {
-	console.log('HeliCtrl Loaded');
+.controller('RootCtrl', ['$scope', '$state', function($scope, $state) {
+	//This function is used to set the active class on the navbar items
+	$scope.isNavActive = function(currentStateName) {
+		return currentStateName === $state.current.name;
+	};
 }]);
